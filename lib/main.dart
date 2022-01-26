1e39e823cc9e55ee4df9d6ff9d4a371d0a5bd05b
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2022-01-26 21:41:07 +0100

    changed to stateful and added setState

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,38 @@ void main(){
  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
   const MyApp({Key? key}) : super(key: key);
 
-  void answerQuestion() {
-    print('Answer chosen');
-  }
 
-   
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+ int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<String> questions = ['What\'s your favourite color?', 'What\'s your facourite animal?'];
+    List<String> questions = ['What\'s your favourite color?', 'What\'s your facourite animal?', 'What\'s your facourite meal?'];
     return  MaterialApp(home: Scaffold(
       appBar: AppBar(title: const Text("Dart and Flutter"),),
       body: Column(children:
        [
-         const Text('The questions!'),
+          Text(questions[questionIndex]),
           ElevatedButton(onPressed: answerQuestion, child: const Text('Answer 1')),
-          ElevatedButton(onPressed: answerQuestion, child: const Text('Answer 2')),
-          ElevatedButton(onPressed: answerQuestion, child: const Text('Answer 3')),
+          ElevatedButton(onPressed: ()=> print('answer 2 chosen'), child: const Text('Answer 2')),
+          ElevatedButton(onPressed: (){print('answer 3 chosen');}, child: const Text('Answer 3')),
       ],)
     ),
     debugShowCheckedModeBanner: false,
     );
   }
-
-  
 }
